772faa80aa43276e19772a452e4023e33e1c77b2
Ophus Wong <ophus.wong@gmail.com>
2018-02-15 14:34:01 -0700

    Style TopNav btns Add icons, hover state, and remove default styling

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -11,20 +11,20 @@ export const SideNav = () => {
     <section className="SideNav">
       <div className="side-nav-user-profile">User Profile Image</div>
       <NavLink to="/home">
-        <button>
-          <FontAwesomeIcon icon={faHome}/>
+        <button className="sidenav-btn">
+          <FontAwesomeIcon icon={faHome} size="2x"/>
           Home
         </button>
       </NavLink>
       <NavLink to="/profile">
-        <button>
-          <FontAwesomeIcon icon={faUser}/>
+        <button className="sidenav-btn">
+          <FontAwesomeIcon icon={faUser} size="2x"/>
           Profile
         </button>
       </NavLink>
       <NavLink to="/settings">
-        <button>
-          <FontAwesomeIcon icon={faCog}/>
+        <button className="sidenav-btn">
+          <FontAwesomeIcon icon={faCog} size="2x"/>
           Settings
         </button>
       </NavLink>
--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './TopNav.scss';
+import './TopNav.css';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faEdit from '@fortawesome/fontawesome-free-solid/faEdit';
+import faUsers from '@fortawesome/fontawesome-free-solid/faUsers';
+import faHistory from '@fortawesome/fontawesome-free-solid/faHistory';
 
 export const TopNav = () => {
   return (
     <nav className="TopNav">
       <NavLink to="/workout">
-        <button>Post Workout</button>
+        <button className="topnav-btn">
+          <FontAwesomeIcon icon={faEdit} size="3x"/>
+          <h4>
+            Post Workout
+          </h4>
+        </button>
       </NavLink>
       <NavLink to="/team">
-        <button>Team</button>
+        <button className="topnav-btn">
+          <FontAwesomeIcon icon={faUsers} size="3x"/>
+          <h4>
+            Team
+          </h4>
+        </button>
       </NavLink>
       <NavLink to="/workout-history">
-        <button>Workout History</button>
+        <button className="topnav-btn">
+          <FontAwesomeIcon icon={faHistory} size="3x"/>
+          <h4>
+            Past Workouts
+          </h4>
+        </button>
       </NavLink>
     </nav>
   )
