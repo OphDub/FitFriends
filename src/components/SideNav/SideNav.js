7f0dc1c786c231a71694b45978079557b974b0f4
Ophus Wong <ophus.wong@gmail.com>
2018-02-15 13:14:48 -0700

    Add fontawesome modules and import into SideNav

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import './SideNav.css';
 import { NavLink } from 'react-router-dom';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faHome from '@fortawesome/fontawesome-free-solid/faHome';
+import faUser from '@fortawesome/fontawesome-free-solid/faUser';
+import faCog from '@fortawesome/fontawesome-free-solid/faCog';
 
 export const SideNav = () => {
   return (
     <section className="SideNav">
       <div className="side-nav-user-profile">User Profile Image</div>
       <NavLink to="/home">
-        <button>Home</button>
+        <button>
+          <FontAwesomeIcon icon={faHome}/>
+          Home
+        </button>
       </NavLink>
       <NavLink to="/profile">
-        <button>Profile</button>
+        <button>
+          <FontAwesomeIcon icon={faUser}/>
+          Profile
+        </button>
       </NavLink>
       <NavLink to="/settings">
-        <button>Settings</button>
+        <button>
+          <FontAwesomeIcon icon={faCog}/>
+          Settings
+        </button>
       </NavLink>
     </section>
   )
