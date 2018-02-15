8579fc1562b8fbed0fa9b1bce50e428d4c3b1bf0
Ophus Wong <ophus.wong@gmail.com>
2018-02-15 12:04:09 -0700

    Add styling and correct test for SignUp

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './SignUp.css';
 
 class SignUp extends Component {
   constructor () {
@@ -19,36 +20,34 @@ class SignUp extends Component {
 
   render () {
     return (
-      <section className="SignUp">
-        <form action="">
-          <input type="text"
-            placeholder="First Name"
-            name="firstName"
-            value={this.state.firstName}
-            onChange={this.handleChange}/>
-          <input type="text"
-            placeholder="Last Name"
-            name="lastName"
-            value={this.state.lastName}
-            onChange={this.handleChange}/>
-          <input type="text"
-            placeholder="Username"
-            name="userName"
-            value={this.state.userName}
-            onChange={this.handleChange}/>
-          <input type="text"
-            placeholder="Email"
-            name="userEmail"
-            value={this.state.userEmail}
-            onChange={this.handleChange}/>
-          <input type="password"
-            placeholder="Password"
-            name="userPass"
-            value={this.state.userPass}
-            onChange={this.handleChange}/>
-          <button>Sign Up</button>
-        </form>
-      </section>
+      <form className="SignUp">
+        <input type="text"
+          placeholder="First Name"
+          name="firstName"
+          value={this.state.firstName}
+          onChange={this.handleChange}/>
+        <input type="text"
+          placeholder="Last Name"
+          name="lastName"
+          value={this.state.lastName}
+          onChange={this.handleChange}/>
+        <input type="text"
+          placeholder="Username"
+          name="userName"
+          value={this.state.userName}
+          onChange={this.handleChange}/>
+        <input type="text"
+          placeholder="Email"
+          name="userEmail"
+          value={this.state.userEmail}
+          onChange={this.handleChange}/>
+        <input type="password"
+          placeholder="Password"
+          name="userPass"
+          value={this.state.userPass}
+          onChange={this.handleChange}/>
+        <button>Sign Up</button>
+      </form>
     )
   }
 }
