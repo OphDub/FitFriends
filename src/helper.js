d8db7888164a5b1bc11976eb4a0f13b35875999c
Ophus Wong <ophus.wong@gmail.com>
2018-02-16 18:26:16 -0700

    Add connectToFitBit to helper and imported into SideNav

--- src/helper.js
+++ src/helper.js
@@ -1,4 +1,15 @@
 import { clientID, clientSecret } from './apiKey';
-import { FitbitApiClient } from 'fitbit-node';
+const root = 'https://www.fitbit.com/oauth2/authorize?'
+const redirect = '&redirect_uri=http://localhost:3000/home';
 
-console.log(FitbitApiClient);
+export const connectToFitBit = () => {
+  const root = 'https://www.fitbit.com/oauth2/authorize?';
+  const redirect = 'http://localhost:3000/home';
+  const scope = 'activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight';
+  const exp = 604800;
+
+  const tokenInfo = window.location.href;
+  window.location = `${root}response_type=code&client_id=${clientID}&redirect_uri=${redirect}&scope=${scope}&expires_in=${exp}`;
+
+  console.log(tokenInfo);
+}
