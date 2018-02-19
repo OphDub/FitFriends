c777932d9c0e87bb8c397ef77b669ea706ec6fa1
Ophus Wong <ophus.wong@gmail.com>
2018-02-19 15:35:54 -0700

    Clean up helper js file

--- src/helper.js
+++ src/helper.js
@@ -1,13 +1,4 @@
-import { clientID, clientSecret } from './apiKey';
-
-export const connectToFitBit = () => {
-  const root = 'https://www.fitbit.com/oauth2/authorize?';
-  const redirect = 'http://localhost:3000/home';
-  const scope = 'activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight';
-  const exp = 604800;
-
-  const tokenInfo = window.location.href;
-  window.location = `${root}response_type=code&client_id=${clientID}&redirect_uri=${redirect}&scope=${scope}&expires_in=${exp}`;
-  debugger;
-  console.log(window.location.href);
+export const connectToFitBit = async () => {
+  const response = await fetch('api/authorize');
+  console.log(response);
 }
