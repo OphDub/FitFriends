380a448c165dda6128e03ce51c5347794a269a2f
Ophus Wong <ophus.wong@gmail.com>
2018-02-19 15:36:31 -0700

    Change to onClick in SideNav

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -12,7 +12,7 @@ export const SideNav = ({ userImage, userName }) => {
   return (
     <section className="SideNav">
       <div className="user-profile">
-        <img src={'http://www.ixgetfit.com/wp-content/uploads/2017/10/Brooke-Wells-350x350-300x300.jpg'} alt="User profile" className="user-pic"/>
+        <img src={userImage} alt="User profile" className="user-pic"/>
         <h4 className={`${userName}`}>Brooke</h4>
       </div>
       <div className="sidenav-btns">
