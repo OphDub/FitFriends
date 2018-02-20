7511b56afc28272e390863cc927fb22d5cd504b0
Ophus Wong <ophus.wong@gmail.com>
2018-02-19 22:57:16 -0700

    Add firebase package and change WorkoutHistory to class

--- src/components/WorkoutHistory/WorkoutHistory.js
+++ src/components/WorkoutHistory/WorkoutHistory.js
@@ -1,38 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './WorkoutHistory.css';
 import { connect } from 'react-redux';
+import { getWorkouts } from '../../actions/actionsIndex';
 
-const WorkoutHistory = ({workouts}) => {
-  const renderedWorkouts = workouts.map((workout) => {
-    const exercises = workout.exercises.map((exercise) => {
-      return(
-        <li className="exercise">
-          <p className="exercise-reps">{exercise.reps}</p>
-          <p className="exercise-name">{exercise.exercise}</p>
-        </li>
+class WorkoutHistory extends Component {
+  constructor () {
+    super();
+  }
+
+  componentDidMount () {
+    this.props.getWorkouts();
+  }
+
+  renderedWorkouts = () => {
+    return this.props.workouts.map((workout) => {
+      const exercises = workout.exercises.map((exercise) => {
+        return(
+          <li className="exercise">
+            <p className="exercise-reps">{exercise.reps}</p>
+            <p className="exercise-name">{exercise.exercise}</p>
+          </li>
+        )
+      })
+
+      return (
+        <article className="workout">
+          <h3 className="workout-name">{workout.workoutName}</h3>
+          <p className="workout-desc">{workout.workoutDesc}</p>
+          <ul className="exercises">
+            {exercises}
+          </ul>
+        </article>
       )
     })
+  }
 
+  render () {
+    console.log(this.props);
     return (
-      <article className="workout">
-        <h3 className="workout-name">{workout.workoutName}</h3>
-        <p className="workout-desc">{workout.workoutDesc}</p>
-        <ul className="exercises">
-          {exercises}
-        </ul>
-      </article>
+      <section className="WorkoutHistory">
+        {this.renderedWorkouts()}
+      </section>
     )
-  })
-
-  return(
-    <section className="WorkoutHistory">
-      {renderedWorkouts}
-    </section>
-  )
+  }
 }
 
 const mapStateToProps = (state) => ({
   workouts: state.workouts,
-});
+})
 
-export default connect(mapStateToProps)(WorkoutHistory);
+const mapDispatchToProps = (dispatch) => ({
+  getWorkouts: () => dispatch(getWorkouts())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(WorkoutHistory);
