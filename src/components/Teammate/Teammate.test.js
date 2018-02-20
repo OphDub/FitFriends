eb33102786349a33fffa41f105409bc8707e246d
Ophus Wong <ophus.wong@gmail.com>
2018-02-20 12:34:12 -0700

    Correct tests for Team and Teammate to shallow properly

---
+++ src/components/Teammate/Teammate.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Teammate } from './Teammate';
+
+describe('TEAMMATE', () => {
+  it('should match snapshot', () => {
+    const renderedComponent = shallow(<Teammate/>);
+
+    expect(renderedComponent).toMatchSnapshot();
+  });
+});
