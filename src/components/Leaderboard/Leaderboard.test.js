1b7d3eb87a1960f22dfd55cc259f6bec279f8039
Ophus Wong <ophus.wong@gmail.com>
2018-02-20 14:07:52 -0700

    Pass mockTeam to Leaderboard component for visual reference

--- src/components/Leaderboard/Leaderboard.test.js
+++ src/components/Leaderboard/Leaderboard.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import {Leaderboard} from './Leaderboard';
+import { Leaderboard } from './Leaderboard';
 
 describe('LEADERBOARD', () => {
   it('should match snapshot', () => {
     const renderedComponent = shallow(<Leaderboard />);
 
     expect(renderedComponent).toMatchSnapshot();
-  })
-})
+  });
+});
