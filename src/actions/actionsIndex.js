48bf5af858bda255cc3a510f56b3810746af058a
Ophus Wong <ophus.wong@gmail.com>
2018-02-19 22:56:39 -0700

    Add getWorkouts actions

--- src/actions/actionsIndex.js
+++ src/actions/actionsIndex.js
@@ -1,4 +1,26 @@
-import { workouts } from '../base';
+// import { workouts } from '../base';
+
+// import Firebase from 'firebase';
+// const workouts = new Firebase('***REMOVED***');
+
+const INITIAL_WORKOUTS = [
+  {
+    workoutName: 'local sample workout name 1',
+    workoutDesc: 'local sample workout desc',
+    exercises: [
+      { reps: 10, exercise: 'sample exercise1'},
+      { reps: 10, exercise: 'sample exercise2'},
+    ]
+  },
+  {
+    workoutName: 'local sample workout name 2',
+    workoutDesc: 'local sample workout desc',
+    exercises: [
+      { reps: 10, exercise: 'sample exercise1'},
+      { reps: 10, exercise: 'sample exercise2'},
+    ]
+  },
+]
 
 export const loginUser = (user) => ({
   type: 'LOGIN_USER',
@@ -15,13 +37,7 @@ export const signUpUser = (user) => ({
   payload: user,
 });
 
-// export const getWorkouts = ({
-//   return dispatch => {
-//     workouts.on('value', snapshot => {
-//       dispatch({
-//         type: 'GET_WORKOUTS',
-//         payload: snapshot.val(),
-//       })
-//     })
-//   }
-// })
+export const getWorkouts = () => ({
+  type: 'GET_WORKOUTS',
+  payload: INITIAL_WORKOUTS
+})
