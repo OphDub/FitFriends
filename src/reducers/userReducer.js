bdbb8fc4663f636ac28f6b2f4d84f300c3642d73
Ophus Wong <ophus.wong@gmail.com>
2018-02-21 13:46:01 -0700

    Add userReducer for user auth with firebase

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -2,6 +2,12 @@ export const userReducer = (state = null, action) => {
   switch (action.type) {
     case 'GET_USER':
       return action.payload;
+    case 'LOGIN_USER':
+      return action.payload;
+    case 'LOGOUT_USER':
+      return action.payload;
+    case 'CREATE_USER':
+      return action.payload;
     default:
       return state;
   }
