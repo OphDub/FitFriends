6d8f2ca9fe93b7442b6b870ca7ae09270bc786fe
Ophus Wong <ophus.wong@gmail.com>
2018-02-24 13:34:14 -0700

    Finish user sign in and out process Remove unneeded actions from actionIndex Remove unneeded reducers from reducersindex Add conditional in App for getUser action

--- src/reducers/reducersIndex.js
+++ src/reducers/reducersIndex.js
@@ -1,14 +1,10 @@
 import { combineReducers } from 'redux';
-import { handleLoginReducer } from './handleLoginReducer';
 import { workoutsReducer } from './workoutsReducer';
-import { signUpUserReducer } from './signUpUserReducer';
 import { userReducer } from './userReducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
-  activeUser: handleLoginReducer,
   workouts: workoutsReducer,
-  newUser: signUpUserReducer,
 });
 
 export default rootReducer;
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,11 +1,9 @@
-export const userReducer = (state = { loggedIn: false }, action) => {
+export const userReducer = (state = { loggedIn: false, email: null }, action) => {
   switch (action.type) {
     case 'GET_USER':
       return { loggedIn: true, email: action.user.email };
-    case 'LOGIN_USER':
-      return action;
     case 'LOGOUT_USER':
-      return action;
+      return { loggedIn: false, email: null };
     case 'CREATE_USER':
       return action;
     default:
