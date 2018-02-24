30367b1361c7ee49b67839425f76c442db51cd45
Ophus Wong <ophus.wong@gmail.com>
2018-02-24 10:48:44 -0700

    Change button in Welcome to Navlink

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Welcome.css';
 import Control from '../Control/Control';
 import { NavLink } from 'react-router-dom';
