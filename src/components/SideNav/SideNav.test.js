9597b93439843fbc77580707cd997ce105a6cc38
Ophus Wong <ophus.wong@gmail.com>
2018-02-25 11:11:10 -0700

    Update tests for SideNav

--- src/components/SideNav/SideNav.test.js
+++ src/components/SideNav/SideNav.test.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { SideNav } from './SideNav';
+import { SideNav, mapStateToProps, mapDispatchToProps } from './SideNav';
 
 describe('SIDENAV', () => {
-  it('should match snapshot', () => {
+  it('should match the snapshot', () => {
     const renderedComponent = shallow(<SideNav />);
 
     expect(renderedComponent).toMatchSnapshot();
+  });
+
+  it('should map the store correctly', () => {
+    const mockUser = { loggedIn: true, name: 'Someuser@me.com'};
+    const mockStore = {
+      user: mockUser
+    };
+
+    const mapped = mapStateToProps(mockStore);
+
+    expect(mapped.user).toEqual(mockStore.user);
+  });
+
+  it('should call the dispatch func when the logout func is called', () => {
+    const mockDispatch = jest.fn();
+    const mapped = mapDispatchToProps(mockDispatch);
+
+    mapped.logout();
+
+    expect(mockDispatch).toHaveBeenCalled();
   });
 });
