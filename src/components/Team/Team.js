7e6413ec00902e2cb6af8ee2ebe4df3e14b0344d
Ophus Wong <ophus.wong@gmail.com>
2018-02-25 17:08:28 -0700

    Lint Team

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import PropTypes, { shape, string, arrayOf } from 'prop-types';
 import './Team.css';
 import { Teammate } from '../Teammate/Teammate';
 
 export const Team = ({team}) => {
-    const renderedTeam = team.map(teammate =>
+  const renderedTeam = team.map(teammate =>
     <Teammate {...teammate} key={teammate.UID}/>
-  )
+  );
 
-  return(
+  return (
     <section className="Team">
       <h3 className="Team-header">Team Members</h3>
       {renderedTeam}
     </section>
-  )
-}
+  );
+};
+
+const teammate = shape({
+  name: string.isRequired,
+  workoutTally: string.isRequired,
+  workoutsPosted: string.isRequired
+});
+
+Team.propTypes = {
+  team: arrayOf(teammate)
+};
