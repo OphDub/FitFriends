79809937223f9d4aed9781644e3bc6760779518b
Ophus Wong <ophus.wong@gmail.com>
2018-02-26 03:25:12 -0700

    Refactor thunk actions and add tests

--- src/actions/actionsIndex.js
+++ src/actions/actionsIndex.js
@@ -1,14 +1,33 @@
 import { workoutsDb } from '../base';
 import { auth } from '../base';
 
-export const getUser = (user) => {
+// export const getUser = (user) => {
+//   return async dispatch => {
+//     await auth.onAuthStateChanged( user => {
+//       dispatch({
+//         type: 'GET_USER',
+//         user
+//       })
+//     })
+//   }
+// };
+
+export const saveUserInStore = (user) => ({
+  type: 'SAVE_USER',
+  user
+});
+
+export const saveUserError = (error) => ({
+  type: 'SAVE_USER_ERROR',
+  error
+});
+
+export const getUserFromFirebase = (user) => {
   return async dispatch => {
-    await auth.onAuthStateChanged( user => {
-      dispatch({
-        type: 'GET_USER',
-        user
-      })
-    })
+    await auth
+            .onAuthStateChanged(user)
+            .then((user) => dispatch(saveUserInStore(user)))
+            .catch((error) => dispatch(saveUserError(error)));
   }
 };
 
@@ -18,13 +37,15 @@ export const login = (email, password) => {
   }
 };
 
+export const logOutUserLocally = () => {
+  type: 'LOGOUT_USER'
+};
+
 export const logout = () => {
   return async dispatch => {
     await auth.signOut();
 
-    dispatch({
-      type: 'LOGOUT_USER',
-    })
+    dispatch({ type: 'LOGOUT_USER' });
   }
 };
 
@@ -38,13 +59,16 @@ export const postWorkout = (workout) => {
   return dispatch => workoutsDb.push(workout)
 };
 
+export const saveWorkoutsInStore = (workouts) => ({
+  type: 'SAVE_WORKOUTS',
+  workouts
+});
+
 export const getWorkouts = () => {
-  return dispatch => {
-    workoutsDb.on('value', snapshot => {
-      dispatch({
-        type: 'GET_WORKOUTS',
-        workouts: snapshot.val()
-      })
+  return async dispatch => {
+    await workoutsDb.on('value', snapshot => {
+
+      dispatch(saveWorkoutsInStore(snapshot.val()));
     })
   }
 };
