177a5315262cb7d54d77bf32743d2eef37622d3c
Ophus Wong <ophus.wong@gmail.com>
2018-02-27 22:20:53 -0700

    Move mstp and mdtp to own describe block for SideNav

--- src/containers/SideNav/SideNav.test.js
+++ src/containers/SideNav/SideNav.test.js
@@ -6,31 +6,37 @@ import { SideNav, mapStateToProps, mapDispatchToProps } from './SideNav';
 describe('SIDENAV', () => {
   it('should match the snapshot', () => {
     const mockImgUrl = 'www.someimage.com';
+    const mockUserName = 'Batman';
+    const mockUserEmail = 'batman@gotham.com'
     const mockUser = {
-      image: mockImgUrl
+      image: mockImgUrl,
+      name: mockUserName,
+      email: mockUserEmail
     }
-    const renderedComponent = shallow(<SideNav user={mockUser}/>);
+    const renderedComponent = shallow(<SideNav user={mockUser}/>, { disableLifecycleMethods: true });
 
     expect(renderedComponent).toMatchSnapshot();
   });
 
-  it('should map the store correctly', () => {
-    const mockUser = { loggedIn: true, name: 'Someuser@me.com'};
-    const mockStore = {
-      user: mockUser
-    };
+  describe('mapStateToProps and mapDispatchToProps for SideNav', () => {
+    it('should map the store correctly', () => {
+      const mockUser = { loggedIn: true, name: 'Someuser@me.com'};
+      const mockStore = {
+        user: mockUser
+      };
 
-    const mapped = mapStateToProps(mockStore);
+      const mapped = mapStateToProps(mockStore);
 
-    expect(mapped.user).toEqual(mockStore.user);
-  });
+      expect(mapped.user).toEqual(mockStore.user);
+    });
 
-  it('should call the dispatch func when the logout func is called', () => {
-    const mockDispatch = jest.fn();
-    const mapped = mapDispatchToProps(mockDispatch);
+    it('should call the dispatch func when the logout func is called', () => {
+      const mockDispatch = jest.fn();
+      const mapped = mapDispatchToProps(mockDispatch);
 
-    mapped.logout();
+      mapped.logout();
 
-    expect(mockDispatch).toHaveBeenCalled();
+      expect(mockDispatch).toHaveBeenCalled();
+    });
   });
 });
